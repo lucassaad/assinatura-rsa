eae97d6c9f73d89e9f5543baa6ffa0bb761bf780
Lucas Saad Rodrigues <lucassaadro@gmail.com>
2025-07-13 23:06:57 -0300

    feat(serializacao): adiciona serializacao e armazenamento das chaves

--- geracao_chaves_e_cifra/geracao_chaves.py
+++ geracao_chaves_e_cifra/geracao_chaves.py
@@ -3,27 +3,6 @@ from geracao_primo import geracao_num_primo
 
 BITS_CHAVE = 1024
 
-def get_primos(num_bits):
-    first = geracao_num_primo(num_bits)
-    second = geracao_num_primo(num_bits)
-    return (first, second)
-
-def get_mdc(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
-def get_expoente_publico(k):
-    while True:
-        e = random.randrange(2, k)
-        mdc = get_mdc(e, k)
-        if mdc == 1:
-            return e
-
-def get_expoente_privado(k, e):
-    d = pow(e, -1, k)
-    return d
-
 def geracao_chaves_rsa(num_bits):
     # definir p e q
     p = geracao_num_primo(num_bits)
@@ -42,3 +21,31 @@ def geracao_chaves_rsa(num_bits):
     d = get_expoente_privado(phi_n, e)
 
     return ((e, n), (d, n))
+
+# importante destacar o porque dessa escolha
+def get_expoente_publico(k):
+
+    # while True:
+    #     e = random.randrange(2, k)
+    #     mdc = get_mdc(e, k)
+    #     if mdc == 1:
+    #         return e
+
+    # por padrão utiliza-se o expoente 65537
+    # eficiente para calculo de m**e
+    # seguro
+    return 65537 
+        
+def get_expoente_privado(k, e):
+    d = pow(e, -1, k)
+    return d
+
+def get_primos(num_bits):
+    first = geracao_num_primo(num_bits)
+    second = geracao_num_primo(num_bits)
+    return (first, second)
+
+def get_mdc(a, b):
+    while b:
+        a, b = b, a % b
+    return a
